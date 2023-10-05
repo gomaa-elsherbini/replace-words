66397caf1300c9ba9ef94e2ba84cbf13f6726567
gomaa <gogochemist35@gmail.com>
2023-10-05 17:45:02 +0300

    Add project files.

---
+++ p42 replace words/p42 replace words.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+string readString(string msg)
+{
+	string str;
+	cout << msg<<endl;
+	getline(cin, str);
+	return str;
+}
+
+vector <string> splitString(string str, string delim)
+{
+	vector <string>vStr;
+	string word = "";
+	short pos = 0;
+
+	//"Mohammad Abu-Hadhoud Iam from Jordan"
+	while ((pos = str.find(delim)) != -1)
+	{
+		word = str.substr(0, pos);
+		str = str.erase(0, pos + delim.length());
+		if (word != "")
+			vStr.push_back(word);
+	}
+	if (str != "")
+		vStr.push_back(str);
+
+	return vStr;
+}
+
+void replaceWordInString(vector <string>&vStr, string s1, string s2)
+{
+	for (string& word : vStr)
+	{
+		if (word == s1)
+			word = s2;
+	}
+}
+
+void printVectorStr(vector <string>vStr)
+{
+	string replacedStr = "";
+
+	for (string& word : vStr)
+	{
+		replacedStr += word+" ";
+	}
+	replacedStr = replacedStr.substr(0, replacedStr.length() - 1);
+	cout << replacedStr << endl << endl;
+}
+
+
+int main()
+{
+	string str = readString("Enter your string?");
+	vector <string> vStr= splitString(str, " ");
+	string s1 = readString("Enter the string you want to replace?");
+	string s2 = readString("Enter the string you want to replace to?");
+
+	replaceWordInString(vStr, s1, s2);
+	printVectorStr(vStr);
+
+
+	return 0;
+}
+
+//vector <string> formStrWords(string str)
+//{
+//	vector <string>vStr;
+//	string word = "";
+//	short pos = 0;
+//	string delim = " ";
+//	string S1 = "";
+//	//"Mohammad Abu-Hadhoud Iam from Jordan"
+//	
+//	while ((pos = str.find(delim)) != -1)
+//	{
+//		word = str.substr(0, pos);
+//		str = str.erase(0, pos +delim.length());
+//		if (word != "")
+//			//vStr.push_back(word);
+//			S1 = word + S1;
+//	}
+//	if (str != "")
+//		//vStr.push_back(str);
+//		S1 = str + S1;
+//
+//	return vStr;
+//}
+//void reversStr(string str)
+//{
+//	string word = "";
+//	short pos = 0;
+//	string delim = " ";
+//	string S1 = "";
+//	//"Mohammad Abu-Hadhoud Iam from Jordan"
+//
+//	while ((pos = str.find(delim)) != -1)
+//	{
+//		word = str.substr(0, pos);
+//		str = str.erase(0, pos + delim.length());
+//		if (word != "")
+//			S1 = word +delim+ S1;
+//	}
+//	if (str != "")
+//		S1 = str +delim + S1;
+//
+//	cout << S1 << endl;
+//}
+//void printReverseStr(vector <string>vStr)
+//{
+//	for (int i = vStr.size() - 1; i >= 0; i--)
+//	{
+//		cout << vStr[i] << " ";
+//	}
+//}
